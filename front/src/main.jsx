5fcabc1151319bc4b27221025f6f299a5aef0821
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-06-22 10:19:11 +0100

    wip

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ApolloProvider from "./graphql/ApolloProvider.jsx";
+import UserManagement from "./components/UserManagement.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <ApolloProvider>
+    <UserManagement />
+  </ApolloProvider>,
+);
